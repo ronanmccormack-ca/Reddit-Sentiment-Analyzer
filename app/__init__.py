dfa01172dd1fd9d8c2b6f602f4f37b0502c1b287
Ronan McCormack <78881615+ronanmccormack-ca@users.noreply.github.com>
2024-02-13 14:52:18 -0800

    initial files

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
-# app/__init__.py
 from flask import Flask
 import praw
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
 
 reddit = praw.Reddit(
-    client_id='eJLRIJM0oRMPbp9rQE6DEA',
-    client_secret='fj6fZkTHo5X2m5ybabPSXqCLV66gdg',
-    user_agent='sentiment'
+    client_id=os.getenv('REDDIT_CLIENT_ID'),
+    client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
+    user_agent=os.getenv('REDDIT_USER_AGENT')
 )
 
 from app import views
